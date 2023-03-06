f0de156a5a6155f031e695f140db20ba209c110e
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-06 13:02:30 +0100

    SIMPLIFYING SHELL

--- myconfig.sh
+++ myconfig.sh
@@ -1,18 +1,19 @@
 echo -e "\nPasting the .editorconfig, .vscode/settings.json and .gitignore files in the current directory:"
 pwd
-cp /mnt/c/Users/bcazabat/Desktop/REPOS/editorconfigtpl/.editorconfig .
+configpath=$( dirname -- "$( readlink -f -- "$0"; )"; )
+cp "$configpath"/.editorconfig .
 mkdir -p .vscode
-cp /mnt/c/Users/bcazabat/Desktop/REPOS/editorconfigtpl/.vscode/settings.json .vscode/settings.json
+cp "$configpath"/.vscode/settings.json .vscode/settings.json
 if [ -f .gitignore ]; then
     echo -e "\nAppending the .gitignore file in the current directory with new rules:"
     if grep -q "# Automatic editorconfig + autoformat on save file for Visual Studio Code" .gitignore; then
         echo -e "\n.gitignore rules already present in the .gitignore file. Skipping."
     else
         echo -e "\n.gitignore rules not present in the .gitignore file. Appending it."
-        cat /mnt/c/Users/bcazabat/Desktop/REPOS/editorconfigtpl/.gitignore >> .gitignore
+        cat "$configpath"/.gitignore >> .gitignore
     fi
 else
     echo -e "\nPasting the .gitignore file in the current directory:"
-    cp /mnt/c/Users/bcazabat/Desktop/REPOS/editorconfigtpl/.gitignore .
+    cp "$configpath"/.gitignore .
 fi
 echo -e "\nDone.\n"

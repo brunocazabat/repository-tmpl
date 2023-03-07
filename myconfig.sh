4164215a48670bccec0eb1eea786b1e9537dd75c
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-07 19:41:06 +0100

    FINAL VERSION OF DISTANT TPL SHL

--- myconfig.sh
+++ myconfig.sh
@@ -1,27 +1,31 @@
-echo -e "\nPasting the .editorconfig, .vscode/settings.json and .gitignore files in the current directory:"
+R='\033[0;31m'
+G='\033[0;32m'
+B='\033[0;34m'
+N='\033[0m'
+
+echo -e "\n${B}Start of the project initialisation in the current directory:"
 if [ -f .editorconfig ]; then
-    echo -e "\n.editorconfig file already present in the current directory. Skipping."
+    echo -e "\n${N}.editorconfig file already present in the current directory. ${R}Skipping."
 else
-    echo -e "\n.editorconfig file not present in the current directory. Creating it."
+    echo -e "\n${N}.editorconfig file not present in the current directory. ${G}Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.editorconfig > .editorconfig
 fi
 if [ -f .gitignore ]; then
-    echo -e "\nAppending the .gitignore file in the current directory with new rules:"
     if grep -q "# Automatic editorconfig + autoformat on save file for Visual Studio Code" .gitignore; then
-        echo -e "\n.gitignore rules already present in the .gitignore file. Skipping."
+        echo -e "\n${N}.gitignore rules already present in the .gitignore file. ${R}Skipping."
     else
-        echo -e "\n.gitignore rules not present in the .gitignore file. Appending it."
+        echo -e "\n${N}.gitignore rules not present in the .gitignore file. ${G}Appending it."
         curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.gitignore >> .gitignore
     fi
 else
-    echo -e "\n.gitignore file not present in the current directory. Creating it."
+    echo -e "\n${N}.gitignore file not present in the current directory. ${G}Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.gitignore > .gitignore
 fi
 mkdir -p .vscode
 if [ -f .vscode/settings.json ]; then
-    echo -e "\n.vscode/settings.json file already present in the current directory. Skipping."
+    echo -e "\n${N}.vscode/settings.json file already present in the current directory. ${R}Skipping."
 else
-    echo -e "\n.vscode/settings.json file not present in the current directory. Creating it."
+    echo -e "\n${N}.vscode/settings.json file not present in the current directory. ${G}Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.vscode/settings.json > .vscode/settings.json
 fi
-echo -e "\nDone. Leaving this no-longer-empty place now.\n"
+echo -e "\n${G}Done. Leaving this no-longer-empty place now.${N}\n"

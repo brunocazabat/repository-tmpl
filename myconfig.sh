0c190156443f38f18c9e73e3fcbbf67d3b6aae1d
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-07 19:24:34 +0100

    Update myconfig.sh

--- myconfig.sh
+++ myconfig.sh
@@ -1,7 +1,8 @@
-echo -e "Pasting the .editorconfig, .vscode/settings.json and .gitignore files in the current directory:"
+echo -e "\nPasting the .editorconfig, .vscode/settings.json and .gitignore files in the current directory:"
 if [ -f .editorconfig ]; then
     echo -e "\n.editorconfig file already present in the current directory. Skipping."
 else
+    echo -e "\n.editorconfig file not present in the current directory. Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.editorconfig > .editorconfig
 fi
 if [ -f .gitignore ]; then
@@ -13,12 +14,14 @@ if [ -f .gitignore ]; then
         curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.gitignore >> .gitignore
     fi
 else
+    echo -e "\n.gitignore file not present in the current directory. Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.gitignore > .gitignore
 fi
 mkdir -p .vscode
 if [ -f .vscode/settings.json ]; then
     echo -e "\n.vscode/settings.json file already present in the current directory. Skipping."
 else
+    echo -e "\n.vscode/settings.json file not present in the current directory. Creating it."
     curl -s https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/.vscode/settings.json > .vscode/settings.json
 fi
 echo -e "\nDone. Leaving this no-longer-empty place now.\n"
